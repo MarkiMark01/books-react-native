c9acf84eac9d9373b39fea06e52710f2a837d718
MarkiMark01 <markishgroup@gmail.com>
2024-03-11 13:00:45 +0200

    add Button and platform

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
 export const Login = () => {
   return (
@@ -13,6 +20,9 @@ export const Login = () => {
           <Text style={styles.titleText}>Password</Text>
           <TextInput style={styles.input} secureTextEntry={true} />
         </View>
+        <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+          <Text style={styles.btnTitle}>Log In</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -31,7 +41,7 @@ const styles = StyleSheet.create({
   },
   input: {
     textAlign: "center",
-    borderWidth: 1,
+    borderWidth: 2,
     borderColor: "#F3D88E",
     color: "#F3D88E",
     marginTop: 3,
@@ -42,5 +52,24 @@ const styles = StyleSheet.create({
   },
   form: {
     marginHorizontal: 20,
+  },
+  btn: {
+    height: 40,
+    borderRadius: 20,
+    color: "#001838",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "#F3D88E",
+      },
+      android: {
+        backgroundColor: "#F3D88E",
+      },
+    }),
+  },
+  btnTitle: {
+    fontSize: 22,
   },
 });
