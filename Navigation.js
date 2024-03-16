5c9c9d753cf565d702839a7aaebbfd226c948584
MarkiMark01 <markishgroup@gmail.com>
2024-03-16 13:40:07 +0200

    update Navigation

--- Navigation.js
+++ Navigation.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -9,26 +10,33 @@ import Books from "./screens/books/Books";
 const AuthStack = createStackNavigator();
 const MainTab = createBottomTabNavigator();
 
-export const Navigation = () => {
-  return (
-    <NavigationContainer>
-      {/* <AuthStack.Navigator>
+const toggleRoute = (isAuth) => {
+  if (!isAuth) {
+    return (
+      <AuthStack.Navigator>
         <AuthStack.Screen
           name={"Login"}
           component={Login}
-          // options={{ title: "Login" }}
           options={{ headerShown: false }}
         />
         <AuthStack.Screen
           name={"Register"}
           component={Register}
-          // options={{ title: "Register" }}
           options={{ headerShown: false }}
         />
-      </AuthStack.Navigator> */}
-      <MainTab.Navigator>
-        <MainTab.Screen name="Books" component={Books} />
-      </MainTab.Navigator>
-    </NavigationContainer>
+      </AuthStack.Navigator>
+    );
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen name="Books" component={Books} />
+    </MainTab.Navigator>
   );
+};
+
+export const Navigation = () => {
+  const isUserAuth = false;
+  const routing = toggleRoute(isUserAuth);
+
+  return <NavigationContainer>{routing}</NavigationContainer>;
 };
