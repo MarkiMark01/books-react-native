304837786d2e24f1ec90afc2e24858d9b5a63b42
MarkiMark01 <markishgroup@gmail.com>
2024-03-19 11:33:29 +0200

    create authOperations

---
+++ redux/auth/authOperations.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as api from "../../shared/auth";
+
+export const signup = createAsyncThunk(
+  "auth/signup",
+  async (data, { rejectWithValue }) => {
+    try {
+      const result = await api.signup(data);
+      return result;
+    } catch ({ responce }) {
+      const { status, data } = responce;
+      const error = {
+        status,
+        message: data.message,
+      };
+      return rejectWithValue(error);
+    }
+  }
+);
