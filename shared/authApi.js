8c76bd37b2dca805a35e6e74a59a9f882df947c8
MarkiMark01 <markishgroup@gmail.com>
2024-03-19 11:21:22 +0200

    create authApi

---
+++ shared/authApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const authInstance = axios.create({
+  baseURL: "https://connections-api.herokuapp.com",
+});
+
+export const signup = async (data) => {
+  const { data: result } = await authInstance.post("/users/signup", data);
+  authInstance.defaults.headers.common.authorization = `Bearer ${result.token}`;
+  return result;
+};
