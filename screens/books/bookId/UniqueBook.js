b008a8da26337fb1db040e5fbf9a6b3278d085b7
MarkiMark01 <markishgroup@gmail.com>
2024-04-10 12:52:23 +0300

    added styles to BooksId

--- screens/books/bookId/UniqueBook.js
+++ screens/books/bookId/UniqueBook.js
@@ -45,7 +45,7 @@ const UniqueBook = () => {
       renderItem={renderItem}
       keyExtractor={(item) => item.id.toString()}
       refreshControl={
-        <RefreshControl refreshing={isLoading} onRefresh={() => {}} />
+        <RefreshControl refreshing={isLoading} onRefresh={() => { }} />
       }
     />
   );
@@ -53,15 +53,95 @@ const UniqueBook = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#e1eff9",
+  },
+
+  image: {
+    width: 265,
+    height: 365,
+    borderColor: "#e1eff9",
+    borderWidth: 1,
+    borderColor: "#F3D88E",
+    borderRadius: 20,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  bookTitle: {
+    height: 50,
+    fontSize: 20,
+    fontFamily: "mt-b",
+    marginTop: 10,
+    marginBottom: 5,
+    marginLeft: 15,
+    marginRight: 15,
+    color: "#001838",
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 13,
+    fontFamily: "mt-b",
+    color: "#001838",
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom: 10,
+    textAlign: "justify",
+  },
+  box: {
     justifyContent: "center",
     alignItems: "center",
-  },
-  image: {
-    width: 200,
-    height: 200,
+    width: 365,
+    borderWidth: 2,
+    borderColor: "#F3D88E",
+    borderRadius: 20,
+    backgroundColor: "#fff",
     marginBottom: 10,
   },
+  priceBlock: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginLeft: 20,
+    marginRight: 20,
+    height: 30,
+  },
+  price: {
+    fontSize: 18,
+    fontFamily: "mt-m",
+    marginTop: 5,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  countBlock: {
+    flexDirection: "row",
+    width: "100%",
+    height: 30,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  quantity: {
+    marginLeft: 15,
+    fontSize: 18,
+    fontFamily: "mt-m",
+  },
+  count: {
+    flexDirection: "row",
+    marginRight: 15,
+  },
+  totalBlock: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: 30,
+  },
+  total1: {
+    marginLeft: 15,
+    fontSize: 18,
+    fontFamily: "mt-m",
+  },
+
 });
 
 export default UniqueBook;
